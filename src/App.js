23c1487b2e1a4c5a570061f29222e79b6b87963e
Romaric Zounlome <rzounlom@gmail.com>
2023-10-25 20:28:01 -0400

    complete website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import About from "./components/about/About";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Navbar from "./components//nav/Navbar";
+import Projects from "./components/projects/Projects";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <About />
+      <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/nav/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>LOGO</div>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
